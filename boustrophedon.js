1701e8a85085290a060f30237a1ef1ea6e58e1ca
Branchey <70140890+CZboop@users.noreply.github.com>
2023-08-14 20:51:19 +0100

    added conversion of text to outlines and start of logic to move each line into own group

--- boustrophedon.js
+++ boustrophedon.js
@@ -16,10 +16,44 @@ function splitIntoLines(textSelection) {
         newTextFrame.contents = splitText[i];
     }
 }
-splitIntoLines(currentSelection);
+// splitIntoLines(currentSelection);
 
-// TODO: turning into shapes
-function turnIntoShapes(){}
+// turning text into shapes
+function turnIntoShapes(textSelection){
+    var textOutlines = textSelection.createOutline();
+    var textObjects = textOutlines.compoundPathItems; // note this might not be the way to get items in the group?
+    var previousY;
+    alert(textObjects.length);
+    var groupNum = 1;
+    var currentGroup = currentLayer.groupItems.add();
+    currentGroup.name = "TextGroup1";
+    currentGroup.move( document, ElementPlacement.PLACEATEND );
+    for (var i=0; i < textObjects.length; i++){
+        var currentY = textObjects[i].bottom;
+        if (!previousY){
+            previousY = currentY;
+        }
+        var currentX = textObjects[i].left;
+        // alert("current x: " + currentX + " current y: " + currentY);
+        
+        if (!(-3 < (currentY - previousY) < 3)){ // checking if y is not close/almost the same, may not be best numbers/to hardcode
+            // make new group for the new line of text
+            groupNum += 1;
+            var newGroup = currentLayer.groupItems.add();
+            currentGroup = newGroup;
+            currentGroup.name = "TextGroup" + groupNum.toString();
+            currentGroup.move( document, ElementPlacement.PLACEATEND );
+            // TODO: set the position of the group or just the contents?
+        }
+        // TODO: why is only every other path being moved into the new group?
+        // alert(currentGroup.name);
+        textObjects[i].move(currentGroup, ElementPlacement.PLACEATEND);
+        previousY = currentY;
+    }
+    // need to actively move out of the group one by one
+    // BUT this can potentially help make by each line, can get the Y of each object and make a new group
+}
+turnIntoShapes(currentSelection);
 
 // TODO: reflecting each alternate line
 function reflectAlternate(){}
