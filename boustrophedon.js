692e34e9203981e0a20e0cbea70bd708bd5449b5
Branchey <70140890+CZboop@users.noreply.github.com>
2023-08-15 19:15:10 +0100

    adjusted logic for grouping text into lines to properly handle punctuation, descenders and other chars that may not have similar y positions to others

--- boustrophedon.js
+++ boustrophedon.js
@@ -5,36 +5,52 @@ function turnIntoShapes(textSelection, currentLayer, document){
     // creating outlines from each letter and getting ref to the resulting path items (in a group)
     var textOutlines = textSelection.createOutline();
     var textObjects = textOutlines.compoundPathItems;
-    // init some variables for iteration 
-    var previousY;
+
+    // init variable to use in group name
     var groupNum = 1;
+
     // creating new group in the current layer and adding to the document
     var currentGroup = currentLayer.groupItems.add();
     currentGroup.name = "BoustrophedonTextGroup1";
     currentGroup.move(document, ElementPlacement.PLACEATEND);
+
     // storing initial number of chars in the group as a var, as this will reduce as we move objects out of the group!
-    var textObjectsLength = textObjects.length; 
+    var textObjectsLength = textObjects.length;
+    var previousBottom;
+    var previousTop;
+
     // iterating over each character path, will move to a different group based on Y, intended to make each line a diff group
     for (var i=0; i < textObjectsLength; i++){
-        // iterating but will always want the first elem... as we are removing the current first element/last element evaluated each time
-        var currentY = textObjects[0].geometricBounds[3];
-        if (!previousY){
-            previousY = currentY;
+        // iterating backwards, last index is the first element, always use the last index as group items reducing per iteration, can't use i
+        var lastIndex = textObjects.length - 1;
+        var currentCharBottomY = textObjects[lastIndex].geometricBounds[3];
+        var currentCharTopY = textObjects[lastIndex].geometricBounds[1];
+        
+        // init value for previous y values for comparison to be valid
+        if (!previousBottom){
+            previousBottom = currentCharBottomY;
+            previousTop = currentCharTopY;
         }
-        // checking if y is not close/almost the same, may not be best numbers/to hardcode
-        // TODO: this is not capturing puntuation as being part of the same line
-        if (!(Math.abs(currentY - previousY) < 3)){
-            // make new group for the new line of text and setting it as current group
+
+        // checking if top of current top is below the last bottom as a way of capturing descenders, punctuation etc in a simple rule
+        if (currentCharTopY - previousBottom < -10){
+            // make new group for the new line of text, naming it and setting it as current group
             groupNum += 1;
             var newGroup = currentLayer.groupItems.add();
             currentGroup = newGroup;
             currentGroup.name = "BoustrophedonTextGroup" + groupNum.toString();
             currentGroup.move( document, ElementPlacement.PLACEATEND );
         }
+
         // moving the current item/char out of the original group to the current/new group
-        textObjects[0].move(currentGroup, ElementPlacement.PLACEATEND);
-        previousY = currentY;
+        textObjects[lastIndex].move(currentGroup, ElementPlacement.PLACEATEND);
+        previousBottom = currentCharBottomY;
+        previousTop = currentCharTopY;
     }
+}
+
+function regroupText(){
+    //  TODO:
 }
 
 // reflecting each alternate line
@@ -43,6 +59,7 @@ function reflectAlternate(currentLayer){
     var totalMatrix = app.getScaleMatrix(-100,100);
     // array to store the layers that have group names created in turnIntoShapes func
     var targetGroups = [];
+
     // iterate over groups in layer to get the groups whos name starts with expected
     for (var i = 0; i < currentLayer.groupItems.length; i++) {
         var currentGroupName = currentLayer.groupItems[i].name;
@@ -53,7 +70,8 @@ function reflectAlternate(currentLayer){
             }
         }
     }
-    // iterating over the groups and applying scale matrix to every other group
+
+    // iterating over the groups and applying scale matrix (reflection) to every other group
     for (var i = targetGroups.length - 1; i >= 0; i--){
         // iterating backwards as the groups will be backwards from bottom to top
         if (i % 2 == 1){
